d8261a9f412378a6f3942777a24c95bbf04ae17f
AnBenLa <antonbenlammert@gmail.com>
2020-01-02 22:27:18 +0100

    Worked on SCITIES. Added test, changed method to cover zeros.

--- SCITIES/main.cpp
+++ SCITIES/main.cpp
@@ -1,6 +1,7 @@
 //
 // Created by Mortiferum on 28.12.2019.
-//
+// https://www.spoj.com/problems/SCITIES/
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -11,15 +12,12 @@ using namespace std;
 typedef unsigned short us;
 typedef unsigned int ui;
 
-
-bool maximizing = true;
-bool print_out = false;
+bool maximizing = true, print_out = false;
 us const dim = 101, max_value = 100;
-us m, range;
-us graph[dim][dim], original[dim][dim];
-bool crossed_out_col[dim], crossed_out_row[dim],assigned[dim][dim],
-        assigned_col[dim], assigned_row[dim], active[dim][dim],
-        ocupied_row[dim], ocupied_col[dim];
+us m, range, t, C1, C2, c1, c2, g, graph[dim][dim], original[dim][dim];
+bool    crossed_out[dim][dim],  crossed_out_col[dim],   crossed_out_row[dim],
+        assigned[dim][dim],     assigned_col[dim],      assigned_row[dim],
+        ocupied_row[dim],       ocupied_col[dim];
 ui result;
 
 void print() {
@@ -40,14 +38,12 @@ void sub_row() {
     for (us i = 1; i < range; ++i) {
         us min = max_value;
         for (us j = 1; j < range; ++j) {
-            if (graph[i][j] < min && active[i][j]) {
+            if (graph[i][j] < min) {
                 min = graph[i][j];
             }
         }
         for (us j = 1; j < range; ++j) {
-            if (active[i][j]) {
-                graph[i][j] -= min;
-            }
+            graph[i][j] -= min;
         }
     }
 }
@@ -56,14 +52,12 @@ void sub_col() {
     for (us i = 1; i < range; ++i) {
         us min = max_value;
         for (us j = 1; j < range; ++j) {
-            if (graph[j][i] < min && active[j][i]) {
+            if (graph[j][i] < min) {
                 min = graph[j][i];
             }
         }
         for (us j = 1; j < range; ++j) {
-            if (active[j][i]) {
-                graph[j][i] -= min;
-            }
+            graph[j][i] -= min;
         }
     }
 }
@@ -72,7 +66,7 @@ us find_smallest_entry() {
     us smallest_entry = max_value;
     for (us i = 1; i < range; ++i) {
         for (us j = 1; j < range; ++j) {
-            if (!crossed_out_row[i] && !crossed_out_col[j] && active[i][j] && graph[i][j] < smallest_entry) {
+            if (!crossed_out_row[i] && !crossed_out_col[j] && graph[i][j] < smallest_entry) {
                 smallest_entry = graph[i][j];
             }
         }
@@ -80,33 +74,8 @@ us find_smallest_entry() {
     return smallest_entry;
 }
 
-void sub_from_uncrossed_rows(us smallest_entry) {
-    for (us i = 1; i < range; ++i) {
-        for (us j = 1; j < range; ++j) {
-            if (active[i][j] && !crossed_out_row[i]) {
-                if(graph[i][j] - smallest_entry < 0){
-                    graph[i][j] = 0;
-                } else {
-                    graph[i][j] -= smallest_entry;
-                }
-            }
-        }
-    }
-}
-
-void add_to_crossed_columns(us smallest_entry) {
-    for (us i = 1; i < range; ++i) {
-        for (us j = 1; j < range; ++j) {
-            if (active[i][j] && crossed_out_col[j]) {
-                graph[i][j] += smallest_entry;
-            }
-        }
-    }
-}
-
 us min_lines() {
     us n = 0;
-    bool crossed_out[dim][dim];
     for (us i = 1; i < range; ++i) {
         crossed_out_col[i] = false;
         crossed_out_row[i] = false;
@@ -120,13 +89,13 @@ us min_lines() {
     for (us i = 1; i < range; ++i) {
         bool zero = false;
         for (us j = 1; j < range; ++j) {
-            if (graph[i][j] == 0 && active[i][j] && !crossed_out[i][j]) {
+            if (graph[i][j] == 0 && !crossed_out[i][j]) {
                 if (!zero) {
                     zero = true;
                     assigned_row[i] = true;
                     assigned[i][j] = true;
                     for (us k = 1; k < range; ++k) {
-                        if (graph[k][j] == 0 && active[i][j] && k != i) {
+                        if (graph[k][j] == 0 && k != i) {
                             crossed_out[k][j] = true;
                         }
                     }
@@ -149,7 +118,7 @@ us min_lines() {
         while (!new_marked_rows.empty()) {
             us row = new_marked_rows.front();
             for (us k = 1; k < range; ++k) {
-                if (graph[row][k] == 0 && active[row][k] && !crossed_out_col[k]) {
+                if (graph[row][k] == 0 && !crossed_out_col[k]) {
                     crossed_out_col[k] = true;
                     new_marked_col.push(k);
                     marked = true;
@@ -160,7 +129,7 @@ us min_lines() {
         while (!new_marked_col.empty()) {
             us col = new_marked_col.front();
             for (us i = 1; i < range; ++i) {
-                if (active[i][col] && assigned[i][col] && !crossed_out_row[i]) {
+                if (assigned[i][col] && !crossed_out_row[i]) {
                     crossed_out_row[i] = true;
                     new_marked_rows.push(i);
                     marked = true;
@@ -181,31 +150,85 @@ us min_lines() {
     return n;
 }
 
-bool cover_zeros(us open){
-    if(open == 0)
-        return true;
-    else{
-        for(us i = 1; i < range; ++i){
-            for(us j = 1; j < range; ++j){
-                if(graph[i][j] == 0 && !ocupied_row[i] && !ocupied_col[j]){
-                    ocupied_row[i] = true;
-                    ocupied_col[j] = true;
-                    result += original[i][j];
-                    if(cover_zeros(open - 1)){
-                        return true;
+//TODO to slow!!!
+void cover_zeros(){
+    us covered_zeros = 0;
+    us zero_count_row[dim]{0}, zero_count_col[dim]{0};
+    for(int i = 1; i < range; ++i){
+        for(int j = 1; j < range; ++j){
+            if(graph[i][j] == 0) {
+                zero_count_col[j] += 1;
+                zero_count_row[i] += 1;
+            }
+        }
+    }
+    while(covered_zeros != m){
+        bool single = false;
+        for(int i = 1; i < range; ++i){
+            if(zero_count_row[i] == 1){
+                for(int j = 1; j < range; ++j){
+                    if(graph[i][j] == 0 && zero_count_col[j] == 1){
+                        single = true;
+                        zero_count_row[i] = 0;
+                        zero_count_col[j] = 0;
+                        result += original[i][j];
+                        covered_zeros += 1;
+                        break;
                     }
-                    result -= original[i][j];
-                    ocupied_row[i] = false;
-                    ocupied_col[j] = false;
                 }
             }
         }
-        return false;
-    }
-}
 
-void compute_result() {
-    bool test = cover_zeros(m);
+        if(!single){
+            for(int i = 1; i < range; ++i){
+                if(zero_count_row[i] == 1){
+                    for(int j = 1; j < range; ++j){
+                        if(graph[i][j] == 0){
+                            single = true;
+                            zero_count_row[i] = 0;
+                            zero_count_col[j] = 0;
+                            result += original[i][j];
+                            covered_zeros += 1;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        if(!single){
+            for(int j = 1; j < range; ++j){
+                if(zero_count_col[j] == 1){
+                    for(int i = 1; i < range; ++i){
+                        if(graph[i][j] == 0){
+                            single = true;
+                            zero_count_row[i] = 0;
+                            zero_count_col[j] = 0;
+                            result += original[i][j];
+                            covered_zeros += 1;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        if(!single){
+            for(int i = 1; i < range; ++i){
+                if(zero_count_row[i] != 0){
+                    for(int j = 1; j < range; ++j){
+                        if(graph[i][j] == 0 && zero_count_col[j] != 0){
+                            zero_count_row[i] = 0;
+                            zero_count_col[j] = 0;
+                            result += original[i][j];
+                            covered_zeros += 1;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 void hungarian_algorithm() {
@@ -241,51 +264,42 @@ void hungarian_algorithm() {
         if(print_out)
             print();
     }
-    compute_result();
+    cover_zeros();
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
     ifstream cin("C:\\Users\\Anton\\CLionProjects\\spoj\\SCITIES\\in.txt");
-
-    us t, C1, C2, c1, c2, g;
 
     cin >> t;
 
     while (t--) {
         result = 0;
-        for (us i = 0; i <= dim; ++i) {
-            for (us j = 0; j <= dim; ++j) {
-                graph[i][j] = 0;
+        cin >> C1 >> C2 >> c1 >> c2 >> g;
+        m = max(C1, C2);
+        range = m + 1;
+
+        for (us i = 1; i < range; ++i) {
+            for (us j = 1; j < range; ++j) {
+                if(maximizing)
+                    graph[i][j] = max_value;
+                else
+                    graph[i][j] = 0;
                 original[i][j] = 0;
-                active[i][j] = false;
             }
             ocupied_row[i] = false;
             ocupied_col[i] = false;
         }
-        cin >> C1 >> C2 >> c1 >> c2 >> g;
-        m = max(C1, C2);
-        range = m + 1;
-        while (!(c1 == 0 && c2 == 0 && g == 0)) {
+
+        while (c1+c2+g) {
             if(maximizing){
                 graph[c1][c2] = max_value -  g;
             } else {
                 graph[c1][c2] = g;
             }
             original[c1][c2] = g;
-            active[c1][c2] = true;
             cin >> c1 >> c2 >> g;
-        }
-
-        for(us i = 1; i < range; ++i){
-            for(us j = 1; j < range; ++j){
-                if(!active[i][j]){
-                    graph[i][j] = max_value;
-                    active[i][j] = true;
-                }
-            }
         }
 
         hungarian_algorithm();
