72d5cb9dca87a8f3d6c7cf61039dc5a6768b2a5f
AnBenLa <antonbenlammert@gmail.com>
2020-01-04 16:19:39 +0100

    Worked on SCITIES.

--- SCITIES/main.cpp
+++ SCITIES/main.cpp
@@ -12,12 +12,13 @@ using namespace std;
 typedef unsigned short us;
 typedef unsigned int ui;
 
-bool maximizing = true, print_out = false;
+bool maximizing = false, print_out = false;
 us const dim = 101, max_value = 100;
-us m, range, t, C1, C2, c1, c2, g, graph[dim][dim], original[dim][dim];
-bool    crossed_out[dim][dim],  crossed_out_col[dim],   crossed_out_row[dim],
-        assigned[dim][dim],     assigned_col[dim],      assigned_row[dim],
-        ocupied_row[dim],       ocupied_col[dim];
+us path[dim][2]{0};
+us m, range, t, C1, C2, c1, c2, g, graph[dim][dim], original[dim][dim], assignment [dim][dim]{0};
+bool crossed_out[dim][dim], crossed_out_col[dim], crossed_out_row[dim],
+        covered_row[dim]{false}, covered_col[dim]{false},
+        ocupied_row[dim], ocupied_col[dim];
 ui result;
 
 void print() {
@@ -28,6 +29,24 @@ void print() {
                 cout << graph[i][j] << "\t";
             } else {
                 cout << "*\t";
+            }
+        }
+        cout << "\t\t";
+        for (us j = 1; j < range; ++j) {
+            if (!covered_row[i] && !covered_col[j]) {
+                cout << graph[i][j] << "\t";
+            } else {
+                cout << "*\t";
+            }
+        }
+        cout << "\t\t";
+        for (us j = 1; j < range; ++j) {
+            if (assignment[i][j] == 1) {
+                cout <<"*\t";
+            } else if(assignment[i][j] == 2) {
+                cout << "'\t";
+            } else {
+                cout << "0\t";
             }
         }
         cout << "\n";
@@ -66,7 +85,7 @@ us find_smallest_entry() {
     us smallest_entry = max_value;
     for (us i = 1; i < range; ++i) {
         for (us j = 1; j < range; ++j) {
-            if (!crossed_out_row[i] && !crossed_out_col[j] && graph[i][j] < smallest_entry) {
+            if (!covered_row[i] && !covered_col[j] && graph[i][j] < smallest_entry) {
                 smallest_entry = graph[i][j];
             }
         }
@@ -74,197 +93,190 @@ us find_smallest_entry() {
     return smallest_entry;
 }
 
-us min_lines() {
-    us n = 0;
-    for (us i = 1; i < range; ++i) {
-        crossed_out_col[i] = false;
-        crossed_out_row[i] = false;
-        assigned_col[i] = false;
-        assigned_row[i] = false;
-        for(us j = 1; j < range; ++j){
-            assigned[i][j] = false;
-            crossed_out[i][j] = false;
-        }
-    }
-    for (us i = 1; i < range; ++i) {
-        bool zero = false;
-        for (us j = 1; j < range; ++j) {
-            if (graph[i][j] == 0 && !crossed_out[i][j]) {
-                if (!zero) {
-                    zero = true;
-                    assigned_row[i] = true;
-                    assigned[i][j] = true;
-                    for (us k = 1; k < range; ++k) {
-                        if (graph[k][j] == 0 && k != i) {
-                            crossed_out[k][j] = true;
-                        }
-                    }
-                } else {
-                    crossed_out[i][j] = true;
-                }
-            }
-        }
-    }
-    queue<us> new_marked_rows, new_marked_col;
-    for (us i = 1; i < range; ++i) {
-        if (!assigned_row[i]) {
-            crossed_out_row[i] = true;
-            new_marked_rows.push(i);
-        }
-    }
-    bool marked = true;
-    while (marked) {
-        marked = false;
-        while (!new_marked_rows.empty()) {
-            us row = new_marked_rows.front();
-            for (us k = 1; k < range; ++k) {
-                if (graph[row][k] == 0 && !crossed_out_col[k]) {
-                    crossed_out_col[k] = true;
-                    new_marked_col.push(k);
-                    marked = true;
-                }
-            }
-            new_marked_rows.pop();
-        }
-        while (!new_marked_col.empty()) {
-            us col = new_marked_col.front();
-            for (us i = 1; i < range; ++i) {
-                if (assigned[i][col] && !crossed_out_row[i]) {
-                    crossed_out_row[i] = true;
-                    new_marked_rows.push(i);
-                    marked = true;
-                }
-            }
-            new_marked_col.pop();
-        }
-    }
-    for(us i = 1; i < range; ++i){
-        crossed_out_row[i] = !crossed_out_row[i];
-    }
-    for (us i = 1; i < range; ++i) {
-        if (crossed_out_row[i])
-            n += 1;
-        if (crossed_out_col[i])
-            n += 1;
-    }
-    return n;
-}
-
-//TODO to slow!!!
-void cover_zeros(){
-    us covered_zeros = 0;
-    us zero_count_row[dim]{0}, zero_count_col[dim]{0};
+bool check_matching(){
+    bool matching = true;
     for(int i = 1; i < range; ++i){
         for(int j = 1; j < range; ++j){
-            if(graph[i][j] == 0) {
-                zero_count_col[j] += 1;
-                zero_count_row[i] += 1;
+            if(assignment[i][j] == 1)
+                covered_col[j] = true;
+        }
+    }
+    for(int i = 1; i < range; ++i){
+        if(!covered_col[i])
+            matching = false;
+    }
+    return matching;
+}
+
+int find_path(us i, us j){
+    ui position = 0;
+    path[position][0] = i;
+    path[position][1] = j;
+    bool done = false;
+    while(!done){
+        bool star_in_col = false, prime_in_row = false;
+        for(us i = 1; i < range; ++i){
+            if(assignment[i][path[position][1]] == 1){
+                position += 1;
+                path[position][0] = i;
+                path[position][1] = path[position - 1][1];
+                star_in_col = true;
+                break;
+            }
+        }
+        if(!star_in_col){
+            done = true;
+        }
+        if(!done){
+            for(us k = 1; k < range; ++k){
+                if(assignment[path[position][0]][k] == 2){
+                    position += 1;
+                    path[position][0] = path[position - 1][0];
+                    path[position][1] = k;
+                    prime_in_row = true;
+                    break;
+                }
             }
         }
     }
-    while(covered_zeros != m){
-        bool single = false;
-        for(int i = 1; i < range; ++i){
-            if(zero_count_row[i] == 1){
-                for(int j = 1; j < range; ++j){
-                    if(graph[i][j] == 0 && zero_count_col[j] == 1){
-                        single = true;
-                        zero_count_row[i] = 0;
-                        zero_count_col[j] = 0;
-                        result += original[i][j];
-                        covered_zeros += 1;
-                        break;
-                    }
-                }
+    return position;
+}
+
+void augment_path(ui path_steps){
+    for(ui i = 0; i <= path_steps; ++i){
+        if(assignment[path[i][0]][path[i][1]] == 1)
+            assignment[path[i][0]][path[i][1]] = 0;
+        else
+            assignment[path[i][0]][path[i][1]] = 1;
+    }
+}
+
+void reset_cover(){
+    for(int i = 1; i < range; ++i){
+        covered_col[i] = false;
+        covered_row[i] = false;
+    }
+}
+
+void initial_assignment(){
+    for(us i = 1; i < range; ++i){
+        for(us j = 1; j < range; ++j){
+            if(graph[i][j] == 0 && !covered_row[i] && !covered_col[j]){
+                assignment[i][j] = 1;
+                covered_row[i] = true;
+                covered_col[j] = true;
             }
         }
+    }
+    reset_cover();
+}
 
-        if(!single){
-            for(int i = 1; i < range; ++i){
-                if(zero_count_row[i] == 1){
-                    for(int j = 1; j < range; ++j){
-                        if(graph[i][j] == 0){
-                            single = true;
-                            zero_count_row[i] = 0;
-                            zero_count_col[j] = 0;
-                            result += original[i][j];
-                            covered_zeros += 1;
-                            break;
-                        }
-                    }
+void min_add_sub(){
+    us smallest_entry = find_smallest_entry();
+    for (us i = 1; i < range; ++i) {
+        for (us j = 1; j < range; ++j) {
+            if (!covered_row[i] && !covered_col[j]) {
+                if (graph[i][j] - smallest_entry < 0) {
+                    graph[i][j] = 0;
+                } else {
+                    graph[i][j] -= smallest_entry;
                 }
             }
+            if (covered_row[i] && covered_col[j])
+                graph[i][j] += smallest_entry;
         }
+    }
+}
 
-        if(!single){
-            for(int j = 1; j < range; ++j){
-                if(zero_count_col[j] == 1){
-                    for(int i = 1; i < range; ++i){
-                        if(graph[i][j] == 0){
-                            single = true;
-                            zero_count_row[i] = 0;
-                            zero_count_col[j] = 0;
-                            result += original[i][j];
-                            covered_zeros += 1;
-                            break;
-                        }
-                    }
-                }
-            }
+pair<us,us> process_uncovered_zeros(){
+   for(int i = 1; i < range; ++i){
+       for(int j = 1; j < range; ++j){
+          if(graph[i][j] == 0 && !covered_row[i] && !covered_col[j]){
+              //prime the uncovered zero
+              assignment[i][j] = 2;
+              bool starred_zero_found = false;
+              //check if row contains starred zero
+              for(int k = 1; k < range; ++k){
+                  if(assignment[i][k] == 1){
+                      covered_row[i] = true;
+                      covered_col[k] = false;
+                      starred_zero_found = true;
+                      break;
+                  }
+              }
+              if(!starred_zero_found){
+                  return {i,j};
+              }
+          }
+       }
+   }
+    return {0,0};
+}
+
+void compute_result(){
+    for(int i = 1; i < range; ++i){
+        for(int j = 1; j < range; ++j){
+            if(assignment[i][j] == 1)
+                result += original[i][j];
         }
+    }
+}
 
-        if(!single){
-            for(int i = 1; i < range; ++i){
-                if(zero_count_row[i] != 0){
-                    for(int j = 1; j < range; ++j){
-                        if(graph[i][j] == 0 && zero_count_col[j] != 0){
-                            zero_count_row[i] = 0;
-                            zero_count_col[j] = 0;
-                            result += original[i][j];
-                            covered_zeros += 1;
-                            break;
-                        }
-                    }
-                }
-            }
+void erase_primes(){
+    for(int i = 1; i < range; ++i){
+        for(int j = 1; j < range; ++j){
+            if(assignment[i][j] == 2)
+                assignment[i][j] = 0;
         }
     }
 }
 
 void hungarian_algorithm() {
-    if(print_out)
+    //https://www2.cs.fau.de/teaching/SS2015/HalloWelt/FSB2_2015.pdf
+    //http://csclab.murraystate.edu/~bob.pilgrim/445/munkres.html
+    //star == 1, strich == 2
+
+    if (print_out)
         print();
+
     sub_row();
-    if(print_out)
+
+    if (print_out)
         print();
+
     sub_col();
-    if(print_out)
+
+    if (print_out)
         print();
-    us n = min_lines();
-    if(print_out)
-        print();
-    while (n != m) {
-        us smallest_entry = find_smallest_entry();
-        for(us i = 1; i < range; ++i){
-            for(us j = 1; j < range; ++j){
-                if(!crossed_out_row[i] && !crossed_out_col[j]){
-                    if(graph[i][j] - smallest_entry < 0){
-                        graph[i][j] = 0;
-                    } else {
-                        graph[i][j] -= smallest_entry;
-                    }
-                }
-                if(crossed_out_row[i] && crossed_out_col[j])
-                    graph[i][j] += smallest_entry;
-            }
-        }
-        //sub_from_uncrossed_rows(smallest_entry);
-        //add_to_crossed_columns(smallest_entry);
-        n = min_lines();
+
+    initial_assignment();
+
+    bool finished = false;
+    while(!finished){
         if(print_out)
             print();
+        pair<us,us> unprocessed_zero = process_uncovered_zeros();
+
+        if(print_out)
+            print();
+
+        if(unprocessed_zero.first != 0) {
+            ui steps = find_path(unprocessed_zero.first,unprocessed_zero.second);
+            augment_path(steps);
+            reset_cover();
+            erase_primes();
+
+            if(print_out)
+                print();
+
+            finished = check_matching();
+            continue;
+        }
+
+        min_add_sub();
+
     }
-    cover_zeros();
+    compute_result();
 }
 
 int main() {
@@ -282,19 +294,20 @@ int main() {
 
         for (us i = 1; i < range; ++i) {
             for (us j = 1; j < range; ++j) {
-                if(maximizing)
+                if (maximizing)
                     graph[i][j] = max_value;
                 else
                     graph[i][j] = 0;
                 original[i][j] = 0;
+                assignment[i][j] = 0;
             }
-            ocupied_row[i] = false;
-            ocupied_col[i] = false;
+            covered_col[i] = false;
+            covered_row[i] = false;
         }
 
-        while (c1+c2+g) {
-            if(maximizing){
-                graph[c1][c2] = max_value -  g;
+        while (c1 + c2 + g) {
+            if (maximizing) {
+                graph[c1][c2] = max_value - g;
             } else {
                 graph[c1][c2] = g;
             }
