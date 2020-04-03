a8aeb5755646eb7b2a11ae1b37533e75179adaa5
AnBenLa <antonbenlammert@gmail.com>
2020-04-03 11:38:19 +0200

    Somehow have to repush the Solution for BUSYMAN.

--- BUSYMAN/main.cpp
+++ BUSYMAN/main.cpp
@@ -20,6 +20,21 @@ typedef long long int lli;
 typedef unsigned int ui;
 
 int main() {
-
+    ui t,n,s,e;
+    scanf("%d", &t);
+    while(t--){
+        vector<pair<int,int>> ac, sol;
+        scanf("%d", &n);
+        for(ui i = 0; i < n; ++i){
+            scanf("%d %d",&s,&e);
+            ac.push_back({s,e});
+        }
+        sort(begin(ac), end(ac), [](pair<int,int> a, pair<int,int> b) {return a.second < b.second; });
+        for(ui i = 0; i < n; ++i){
+            if(sol.size() == 0 || ac[i].first >= sol.rbegin()->second)
+                sol.push_back(ac[i]);
+        }
+        printf("%d\n", sol.size());
+    }
     return 0;
 }
